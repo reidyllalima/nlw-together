da4e1dba152f5e1352c4c60f898d306dcde3ab66
Reidylla Lima <reidylla.lima@pop-pe.rnp.br>
2021-07-20 22:51:28 -0300

    menu-abre-fecha

---
+++ main.js
@@ -0,0 +1,19 @@
+/* abre e fecha o menu qnd clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* esconder o menu ao clicar em um item do menu */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
